01c707a8c6418bb090ec9de6ece6c4e1d3aeb9af
Andrey <andrey.tantsyura1980@gmail.com>
2020-10-01 23:52:52 +0300

    add all files

--- calc.js
+++ calc.js
@@ -1,67 +1,62 @@
 var str = '0';
-
+var m = 0
 $('.number').click(function(event) {
 	symbol = $(this).text()
 	$('.display').append(symbol)
 	str += symbol
-console.log(str)
 });
 
 $('.inner_clear').click(function(event) {
 	$('.display').text('')
 });
 
-$('.memory').click(function(event) {
-	console.log(str)
+$('.memory_plus').click(function(event) {
+	m = String($('.display').text())
+	$('.display').text('')
+});
+
+$('.memory_r').click(function(event) {
+	$('.display').append(m)
 });
 
 $('.radical').click(function(event) {
 	str = Math.sqrt(Number(str))
 	$('.display').text(str)
-	console.log(str)
 });
 
 $('.square').click(function(event) {
 	str = Math.pow(Number(str), 2)
 	$('.display').text(str)
-	console.log(str)
 });
 
 $('.plus').click(function(event) {
 		$('.display').append('+')
 	str += '+'
-	console.log(str)
-	$(thit).removeClass('active')
 });
 
 $('.minus').click(function(event) {
 		$('.display').append('-')
 	str += '-'
-	console.log(str)
 });
 
 $('.multiply').click(function(event) {
 		$('.display').append('*')
 	str += '*'
-	console.log(str)
 });
 
 $('.divide').click(function(event) {
 		$('.display').append('/')
 	str += '/'
-	console.log(str)
 });
 
 $('.dot').click(function(event) {
 		$('.display').append('.')
 	str += '.'
-	console.log(str)
 });
 
 $('.iner_result').click(function(event) {
 	str = eval(str)
 	$('.display').text(str)
-		console.log(str)
 });
 
 
