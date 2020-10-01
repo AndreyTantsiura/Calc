26370fc1464434801044ee2c4d50c108978c2f06
Andrey <andrey.tantsyura1980@gmail.com>
2020-10-01 14:14:56 +0300

    add all files

--- calc.js
+++ calc.js
@@ -1,18 +1,45 @@
-var symbol = '';
+var str = '';
 
 $('.number').click(function(event) {
 	symbol = $(this).text()
 	$('.display').append(symbol)
-});
-
-$('.plus').one('click', function(event) {
-	$('.display').append('+')
-});
-
-$('.iner_result').one('click', function(event) {
-	$('.display').val()
+	str += symbol
+console.log(str)
 });
 
 $('.inner_clear').click(function(event) {
+	str = '';
 	$('.display').text(' ')
 });
+
+$('.plus').click(function(event) {
+		$('.display').append('+')
+	str += '+'
+	console.log(str)
+});
+
+$('.minus').click(function(event) {
+		$('.display').append('-')
+	str += '-'
+	console.log(str)
+});
+
+$('.multiply').click(function(event) {
+		$('.display').append('*')
+	str += '*'
+	console.log(str)
+});
+
+$('.divide').click(function(event) {
+		$('.display').append('/')
+	str += '/'
+	console.log(str)
+});
+
+$('.iner_result').click(function(event) {
+	str = eval(str)
+	$('.display').text(str)
+		console.log(str)
+});
+
+
